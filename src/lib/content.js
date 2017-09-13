7f18f4f70c3a8af7b95ac91efaf690e32124d8db
Marc Hanley <marcjameshanley@gmail.com>
2017-09-13 11:02:34 +0930

    Add basic ad deletion

---
+++ src/lib/content.js
@@ -0,0 +1,5 @@
+//Get all browser elements to delete
+
+//Deletes the ads below 'Trending' container
+var element = document.querySelector('div.ego_section');
+element.parentElement.removeChild(element);

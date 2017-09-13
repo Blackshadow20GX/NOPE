e22d1a88218293545714c2659627a6580c157e22
Marc Hanley <marcjameshanley@gmail.com>
2017-09-13 14:08:30 +0930

    Add gitignore, remove nonfunctional code for now

--- src/lib/nope.js
+++ src/lib/nope.js
@@ -1,21 +1,22 @@
-//Deletes the ads below 'Trending' container
-var element = document.querySelectorAll('div.ego_section');
-console.log('Found ' + element.length + ' Ego ads!')
-for (var i = 0; i < element.length; i++){
-    element[i].parentElement.removeChild(element[i]);
-    console.log("NOPE'd Ego Ad#" + (i + 1))
-}
 
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 var observer = new MutationObserver(function(mutations, observer) {
     // fired when a mutation occurs
     console.log(mutations, observer);
     //Deletes the sponsored post containers maybe
-    element = document.querySelectorAll('div.[is_sponsored][is_sponsored=1]');
-    console.log('Found ' + element.length + ' Sponsored ads!')
-    for (var i in element){
+    //element = document.querySelectorAll('a');
+    //var contents = element.innerText();
+    //console.log('Found ' + element.length + ' Sponsored ads!')
+    //for (var i = 0; i < element.length; i++){
+      //  element[i].parentElement.removeChild(element[i]);
+      //  console.log("NOPE'd Sponsored Ad#" + (i + 1));
+    //}
+
+    var element = document.querySelectorAll('div.ego_section');
+    console.log('Found ' + element.length + ' Ego ads!')
+    for (var i = 0; i < element.length; i++){
         element[i].parentElement.removeChild(element[i]);
-        console.log("NOPE'd Sponsored Ad#" + (i + 1));
+        console.log("NOPE'd Ego Ad#" + (i + 1))
     }
 });
 

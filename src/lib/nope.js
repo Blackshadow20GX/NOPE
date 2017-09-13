1ecd2e7774f102e8fc1823e1f82755112eea0c2f
Anthony <anthony.bruno196@gmail.com>
2017-09-13 12:02:45 +0000

    Update to use let/const
    Probably should test this just in case, lol

--- src/lib/nope.js
+++ src/lib/nope.js
@@ -1,11 +1,11 @@
 
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-var observer = new MutationObserver(function(mutations, observer) {
+const observer = new MutationObserver(function(mutations, observer) {
     console.log(mutations, observer);
 
-    var element = document.querySelectorAll('div.ego_section');
+    const element = document.querySelectorAll('div.ego_section');
     console.log('Found ' + element.length + ' Ego ads!')
-    for (var i = 0; i < element.length; i++){
+    for (let i = 0; i < element.length; i++){
         element[i].parentElement.removeChild(element[i]);
         console.log("NOPE'd Ego Ad#" + (i + 1))
     }

4f92c4774c53b79cd67cdb82358528ef8e2dacb0
Marc Hanley <marcjameshanley@gmail.com>
2017-09-19 11:22:37 +0930

    Fix code spacing to 2 spaces

--- src/lib/nope.js
+++ src/lib/nope.js
@@ -6,12 +6,12 @@ const observer = new MutationObserver(function(mutations, observer) {
   console.log(mutations);
 
   mutations.forEach(function(mutation){
-      console.log("Mutation target: " + mutation.target);
-      if(mutation.target.className.indexOf('ego_unit') !== -1){
-          console.log("Ego Unit mutation target found!");
-          console.log("Initiating DOM scan...");
-          domScan(EGO_UNIT_QUERY, EGO_UNIT_TYPE);
-      };
+    console.log("Mutation target: " + mutation.target);
+    if(mutation.target.className.indexOf('ego_unit') !== -1){
+      console.log("Ego Unit mutation target found!");
+      console.log("Initiating DOM scan...");
+      domScan(EGO_UNIT_QUERY, EGO_UNIT_TYPE);
+    };
   });
 });
 
@@ -19,13 +19,13 @@ const observer = new MutationObserver(function(mutations, observer) {
 //@query The target text of the particular element desired
 //@ad_type The type of ad, used in console logging for clarity
 function domScan(query, ad_type){
-    const element = document.querySelectorAll(query);
-    console.log("Found " + element.length + " " + ad_type + " ads!")
-    for (let i = 0; i < element.length; i++){
-        element[i].parentElement.removeChild(element[i]);
-        console.log("NOPE'd " + ad_type + " Ad#" + (i + 1))
-    };
-} 
+  const element = document.querySelectorAll(query);
+  console.log("Found " + element.length + " " + ad_type + " ads!")
+  for (let i = 0; i < element.length; i++){
+    element[i].parentElement.removeChild(element[i]);
+    console.log("NOPE'd " + ad_type + " Ad#" + (i + 1))
+  };
+}; 
 
 //Main processing begins here
 domScan(EGO_UNIT_QUERY, EGO_UNIT_TYPE);
